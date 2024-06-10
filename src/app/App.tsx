be0297786886792fb6f49615dcc5eb4817589d14
everdev <ever.jrg@gmail.com>
2024-06-10 15:46:25 -0400

    Init

---
+++ src/app/App.tsx
@@ -0,0 +1,26 @@
+import {Suspense} from 'react'
+import {Outlet} from 'react-router-dom'
+import {I18nProvider} from '../_zeus/i18n/i18nProvider'
+import {LayoutProvider, LayoutSplashScreen} from '../_zeus/layout/core'
+import {MasterInit} from '../_zeus/layout/MasterInit'
+import {AuthInit} from './modules/auth'
+import {ThemeModeProvider} from '../_zeus/partials'
+
+const App = () => {
+  return (
+    <Suspense fallback={<LayoutSplashScreen />}>
+      <I18nProvider>
+        <LayoutProvider>
+          <ThemeModeProvider>
+            <AuthInit>
+              <Outlet />
+              <MasterInit />
+            </AuthInit>
+          </ThemeModeProvider>
+        </LayoutProvider>
+      </I18nProvider>
+    </Suspense>
+  )
+}
+
+export {App}
