ad732290fec6c06c5a4acc723bda4928be90558c
everdev <ever.jrg@gmail.com>
2024-06-18 18:50:57 -0400

    Saved jun18 18:50

--- src/_zeus/layout/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
+++ src/_zeus/layout/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
@@ -36,8 +36,6 @@ const SidebarMenuMain = () => {
           >
 
               <SidebarMenuItem to='' icon='people' title='Gestión de empleados' fontIcon='bi-layers'/>
-              <SidebarMenuItem to='' icon='user-tick' title='Gestión de beneficios' fontIcon='bi-layers'/>
-              <SidebarMenuItem to='' icon='profile-user' title='Gestión del desempeño' fontIcon='bi-layers'/>
               <SidebarMenuItem to='' icon='profile-circle' title='Reclutamiento y selección' fontIcon='bi-layers'/>
               <SidebarMenuItem to='' icon='delivery-time' title='Seguimiento' fontIcon='bi-layers'/>
               <SidebarMenuItem to='' icon='category' title='Autoservicio' fontIcon='bi-layers'/>
@@ -49,14 +47,77 @@ const SidebarMenuMain = () => {
           <SidebarMenuItem to='' icon='security-user' title='Seguridad y cumplimiento' fontIcon='bi-layers'/>
           <SidebarMenuItem to='' icon='teacher' title='Formación y competencias' fontIcon='bi-layers'/>
 
-
           <div className='menu-item'>
               <div className='menu-content pt-8 pb-2'>
-                  <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Gestión de Calidad </span>
+                  <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Gestión de Procesos</span>
               </div>
           </div>
 
-          <SidebarMenuItem to='' icon='document' title='Gestión de documentos' fontIcon='bi-layers'/>
+          <SidebarMenuItem to='' icon='graph-3' title='Procesos' fontIcon='bi-layers'/>
+          <SidebarMenuItem to='' icon='graph-3' title='Procesos' fontIcon='bi-layers'/>
+          <SidebarMenuItem to='' icon='graph-3' title='Procesos' fontIcon='bi-layers'/>
+
+          <SidebarMenuItemWithSub
+              to=''
+              title='Modelado de Procesos'
+              fontIcon='bi-archive'
+              icon='document'
+          >
+
+              <SidebarMenuItem to='' icon='document' title='Diagramas de Flujo' fontIcon='bi-layers'/>
+              <SidebarMenuItem to='' icon='document' title='Simulación de Procesos' fontIcon='bi-layers'/>
+
+          </SidebarMenuItemWithSub>
+
+          <div className='menu-item'>
+              <div className='menu-content pt-8 pb-2'>
+                  <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Auditorías</span>
+              </div>
+          </div>
+
+          <SidebarMenuItem to='' icon='graph-3' title='Auditorías' fontIcon='bi-layers'/>
+          <SidebarMenuItem to='' icon='data' title='Seguimiento y Análisis de Tendencias' fontIcon='bi-layers'/>
+          <SidebarMenuItem to='' icon='security-user' title='Seguridad y Auditoría de Auditorías' fontIcon='bi-layers'/>
+
+          <div className='menu-item'>
+              <div className='menu-content pt-8 pb-2'>
+                  <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Gestión Documental</span>
+              </div>
+          </div>
+
+          <SidebarMenuItemWithSub
+              to=''
+              title='Gestión de Políticas'
+              fontIcon='bi-archive'
+              icon='document'
+          >
+
+              <SidebarMenuItem to='' icon='document' title='Politicas ambientales' fontIcon='bi-layers'/>
+              <SidebarMenuItem to='' icon='document' title='Politicas de calidad' fontIcon='bi-layers'/>
+              <SidebarMenuItem to='' icon='document' title='Politicas de salud y seguridad ocupacional'
+                               fontIcon='bi-layers'/>
+              <SidebarMenuItem to='' icon='document' title='Politicas de gestion de energia' fontIcon='bi-layers'/>
+
+          </SidebarMenuItemWithSub>
+
+
+          <SidebarMenuItem to='' icon='document' title='Procesos y Procedimientos' fontIcon='bi-layers'/>
+
+          <SidebarMenuItemWithSub
+              to=''
+              title='Gestión de Políticas'
+              fontIcon='bi-archive'
+              icon='document'
+          >
+
+              <SidebarMenuItem to='' icon='document' title='Politicas ambientales' fontIcon='bi-layers'/>
+              <SidebarMenuItem to='' icon='document' title='Politicas de calidad' fontIcon='bi-layers'/>
+              <SidebarMenuItem to='' icon='document' title='Politicas de salud y seguridad ocupacional'
+                               fontIcon='bi-layers'/>
+              <SidebarMenuItem to='' icon='document' title='Politicas de gestion de energia' fontIcon='bi-layers'/>
+
+          </SidebarMenuItemWithSub>
+
           <SidebarMenuItem to='' icon='folder-up' title='Auditorías internas' fontIcon='bi-layers'/>
           <SidebarMenuItem to='' icon='folder-up' title='Gestión de riesgos' fontIcon='bi-layers'/>
           <SidebarMenuItem to='' icon='folder-up' title='Gestión de proveedores' fontIcon='bi-layers'/>
