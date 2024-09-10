bb2a3d7119078c8ea1862eec9f54674e222be8a3
SmartProject10 <isos.smart.osh@gmail.com>
2024-09-10 14:14:05 -0600

    refactor: Update API URL in _requests module

--- src/app/modules/human-resources/tools/calendar/core/_requests.tsx
+++ src/app/modules/human-resources/tools/calendar/core/_requests.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { EmployeeRequest } from "./_models";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_APP_API_URL;
 
 export const REGISTER_EMPLOYEE_URL = `${BASE_URL}/api/trabajador`;
 export const GET_EMPLOYEES_URL = `${BASE_URL}/api/trabajadores`;
