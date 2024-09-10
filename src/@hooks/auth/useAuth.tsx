7dc3fdb3ebe603032d8b32b33a0cbd73e92001ed
SmartProject10 <isos.smart.osh@gmail.com>
2024-09-10 17:29:23 -0600

    feat: Add useAuth hook and AuthProvider component

---
+++ src/@hooks/auth/useAuth.tsx
@@ -0,0 +1,88 @@
+import { Dispatch, FC, PropsWithChildren, SetStateAction, createContext, useContext, useEffect, useState } from 'react'
+
+/* eslint-disable react-refresh/only-export-components */
+import { backyService } from '@zeus/@services/api'
+import * as authHelper from '@zeus/@services/session'
+import { AuthModel } from '@zeus/@services/api/dtos/AuthModel'
+import { LayoutSplashScreen } from '@zeus/_zeus/layout/core'
+import { UserModel } from '@zeus/app/modules/_auth'
+
+type AuthContextProps = {
+    auth: AuthModel | undefined
+    saveAuth: (auth: AuthModel | undefined) => void
+    currentUser: UserModel | undefined
+    setCurrentUser: Dispatch<SetStateAction<UserModel | undefined>>
+    logout: () => void
+}
+
+const initAuthContextPropsState = {
+    auth: authHelper.getAuth(),
+    saveAuth: () => { },
+    currentUser: undefined,
+    setCurrentUser: () => { },
+    logout: () => { },
+}
+
+const AuthContext = createContext<AuthContextProps>(initAuthContextPropsState)
+
+export const useAuth = () => useContext(AuthContext)
+
+export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
+    const [auth, setAuth] = useState<AuthModel | undefined>(authHelper.getAuth())
+    const [currentUser, setCurrentUser] = useState<UserModel | undefined>()
+    const saveAuth = (auth: AuthModel | undefined) => {
+        setAuth(auth)
+        if (auth) {
+            authHelper.setAuth(auth)
+        } else {
+            authHelper.removeAuth()
+        }
+    }
+
+    const logout = () => {
+        saveAuth(undefined)
+        setCurrentUser(undefined)
+    }
+
+    return (
+        <AuthContext.Provider value={{ auth, saveAuth, currentUser, setCurrentUser, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const AuthInit: FC<PropsWithChildren> = ({ children }) => {
+    const { auth, currentUser, logout, setCurrentUser } = useAuth()
+    const [showSplashScreen, setShowSplashScreen] = useState(true)
+
+    // We should request user by authToken (IN OUR EXAMPLE IT'S API_TOKEN) before rendering the application
+    useEffect(() => {
+        const requestUser = async () => {
+            try {
+                if (!currentUser) {
+                    const { data } = await backyService.auth.verifyToken()
+                    if (data) {
+                        setCurrentUser(data)
+                    }
+                }
+            } catch (error) {
+                console.error(error)
+                if (currentUser) {
+                    logout()
+                }
+            } finally {
+                setShowSplashScreen(false)
+            }
+        }
+
+        if (auth && auth.token) {
+            requestUser()
+        } else {
+            logout()
+            setShowSplashScreen(false)
+        }
+        // eslint-disable-next-line
+    }, [])
+
+    return showSplashScreen ? <LayoutSplashScreen /> : <>{children}</>
+}
