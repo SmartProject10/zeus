a8b50f4d5dca1726dd93ec1263472df60cc9047c
SmartProject10 <isos.smart.osh@gmail.com>
2024-09-10 19:11:33 -0600

    feat: Add AuthModel interface and related types
    This commit adds the AuthModel interface and related types to the API DTOs. These types define the structure of the authentication data, user address, communication preferences, email settings, social networks, and user details.

---
+++ src/@services/api/types/apiService.types.ts
@@ -0,0 +1,9 @@
+import { AxiosInstance } from 'axios'
+
+export type SConstructor<T = {}> = new (...args: any[]) => T
+
+export type IService = {
+    http: AxiosInstance
+    // config: BaseConfig
+    debug?: boolean
+}
