d0565837b191c8e5d7b075966128fa32f6382a65
Walter Corrales <corraleswalter@live.com>
2024-09-12 17:07:11 -0600

    Refactor titleBar component styles and import in TitleBar component

---
+++ src/@components/titleBar/index.tsx
@@ -0,0 +1,31 @@
+import { KTIcon } from '@zeus/_zeus/helpers';
+import './index.scss'
+import { useNavigate } from 'react-router-dom';
+import { memo, useCallback } from 'react';
+import { Title } from 'chart.js';
+
+interface TitleBarProps {
+    label: string;
+}
+
+export const TitleBar = memo((props: TitleBarProps): JSX.Element => {
+    const navigate = useNavigate();
+
+    const handleBack = useCallback(() => {
+        navigate(-1);
+    }, [navigate])
+
+    return (
+        <div className="title-bar d-flex flex-row align-items-center gap-4">
+            <button className='btn m-0 p-0' onClick={handleBack}>
+                <KTIcon iconName='arrow-left' className='back-icon' />
+            </button>
+
+            <p className='fs-1 fw-bold m-0'>{props.label}</p>
+        </div>
+    );
+}, (prevProps, nextProps) => {
+    return prevProps.label === nextProps.label;
+})
+
+TitleBar.displayName = 'TitleBar';
