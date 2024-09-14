906b56d98948baaca1cb03d0d39b9ea56efe8f09
Walter Corrales <corraleswalter@live.com>
2024-09-14 14:35:51 -0600

    Add LucesEmergencia page to ISO45001 routes

--- src/app/modules/iso45001/iso45001.routes.tsx
+++ src/app/modules/iso45001/iso45001.routes.tsx
@@ -1,16 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { InspeccionesInternasSeguridad } from './pages/inspeccionesInternasSeguridad'
 import { CompromisosAltaGenerencia } from './pages/compromisosAltaGerencia'
+import { LucesEmergencia } from './pages/lucesEmergencia'
 
 export function ISO45001Routes(): JSX.Element {
-    return (
-        <Routes>
-            <Route
-path="inspecciones-internas-seguridad"
-element={<InspeccionesInternasSeguridad />} />
-            <Route
-path="compromisos-alta-generencia"
-element={<CompromisosAltaGenerencia />} />
-        </Routes>
-    )
+	return (
+		<Routes>
+			<Route path="inspecciones-internas-seguridad" element={<InspeccionesInternasSeguridad />} />
+			<Route path="compromisos-alta-generencia" element={<CompromisosAltaGenerencia />} />
+			<Route path="luces-emergencia" element={<LucesEmergencia />} />
+		</Routes>
+	)
 }
---
+++ src/app/modules/iso45001/pages/lucesEmergencia.tsx
@@ -0,0 +1,9 @@
+import { TitleBar } from '@zeus/@components/titleBar'
+
+export function LucesEmergencia(): JSX.Element {
+	return (
+		<div>
+			<TitleBar label="Luces de emergencia" />
+		</div>
+	)
+}
