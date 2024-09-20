740e0a1dd99080968a733017b9244b109f9afc79
José Gregorio Materán <josemateran1998@gmail.com>
2024-09-20 15:30:10 -0400

    fix: linter

--- src/app/modules/iso45001/pages/kit/core/_models.tsx
+++ src/app/modules/iso45001/pages/kit/core/_models.tsx
@@ -1,10 +1,10 @@
 export interface KitResponse {
-	_id: string;
-	codigo: string;
-	sede: string;
-	numero: number;
-	area: string;
-	ubicacion: string;
-	createdAt: string; //TO ISO STRING
-	updatedAt: string; //TO ISO STRING
+	_id: string
+	codigo: string
+	sede: string
+	numero: number
+	area: string
+	ubicacion: string
+	createdAt: string //TO ISO STRING
+	updatedAt: string //TO ISO STRING
 }
